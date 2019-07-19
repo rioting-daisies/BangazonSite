de9a09a4a5f121688fb67eef5dbb0380ca17114a
greenskybluephish <greenskybluephish@gmail.com>
2019-07-19 15:48:44 -0500

    css for Alex

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -17,14 +17,14 @@ namespace Bangazon.Models
         public DateTime DateCreated {get;set;}
 
         [Required]
-        [StringLength(255)]
-        [RegularExpression(@"^[^!@#$%^&*()]+$", ErrorMessage ="No special charaters are allowed")]
-        public string Description { get; set; }
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
+        [RegularExpression(@"^[^!@#$%^&*()]+$", ErrorMessage = "No special characters are allowed")]
+        public string Title { get; set; }
 
         [Required]
-        [StringLength(55, ErrorMessage="Please shorten the product title to 55 characters")]
-        [RegularExpression(@"^[^!@#$%^&*()]+$", ErrorMessage = "No special charaters are allowed")]
-        public string Title { get; set; }
+        [StringLength(255)]
+        [RegularExpression(@"^[^!@#$%^&*()]+$", ErrorMessage ="No special characters are allowed")]
+        public string Description { get; set; }
 
         [Required]
         [Range(0, 10000.00)]
